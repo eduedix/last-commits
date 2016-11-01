88f045bbffe50a303986e8cd9a19ce4bcd1173e4
eduedix <eduedix@gmail.com>
2016-11-01 12:34:51 +0100

    wrap App inside MuiThemeProvider, put TextField for repo entry

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import TextField from 'material-ui/TextField';
+
+const App = () => (
+  <MuiThemeProvider>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
-    );
-  }
-}
+      <TextField
+        hintText='Enter Repo'
+      />
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  </MuiThemeProvider>
+)
 
 export default App;
