8caa9bf3ba42149fcfd4c986ae24d127d43016f8
eduedix <eduedix@gmail.com>
2016-11-01 14:38:46 +0100

    wrap App with redux's Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App';
 import './index.css';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+const middleware = [ thunk ];
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware),
+)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
