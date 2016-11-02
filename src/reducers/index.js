3659d9ba5f41246ca89afdde3101bc215667e6e6
eduedix <eduedix@gmail.com>
2016-11-02 13:44:07 +0100

    add reducer for repo requests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,17 +2,31 @@ import { combineReducers } from 'redux';
 import { CHANGE_GITHUB_USERNAME, REQUEST_REPOS,
    RECEIVE_REPOS } from '../actions';
    
-const lastCommits = (state = '', action) => {
+const githubUsername = (state = '', action) => {
     switch (action.type) {
         case CHANGE_GITHUB_USERNAME:
-            return action.githubUserName
+            return action.githubUsername
+        case REQUEST_REPOS:
+            return action.githubUsername
+        default:
+            return state
+    }
+}
+
+const repos = (state = [], action) => {
+    switch (action.type) {
+        case REQUEST_REPOS:
+            return []
+        case RECEIVE_REPOS:
+            return action.repos
         default:
             return state
     }
 }
 
 const rootReducer = combineReducers({
-   lastCommits,
+   githubUsername,
+   repos,
 })
 
 export default rootReducer
