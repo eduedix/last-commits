cf9c133786877222fd84cb4a94960b8dab9090ac
eduedix <eduedix@gmail.com>
2016-11-06 12:36:03 +0100

    add utils for random color generation for charts

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+export const generateRandomColor = () => {
+  const r = Math.floor(Math.random() * 200);
+  const g = Math.floor(Math.random() * 200);
+  const b = Math.floor(Math.random() * 200);
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
